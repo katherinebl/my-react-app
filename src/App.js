ad21eeb8c968daa1d7d6f92cc59db6b5cc3c2601
katherinebl <kath.gbl@gmail.com>
2020-05-19 18:19:24 +0200

    Object list example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import ConditionalBlock from './sections/conditionals';
-import logo from './logo.svg';
+import cars from './data/cars.json'
 import './App.css';
+
 
 //1st way of create a React component / Regular function
 // function HELLO (props) {
@@ -15,15 +15,25 @@ import './App.css';
 //Importante: dar contexto con 'this' porque render no acepta parámetros y, por lo tanto, no podemos pasarle las props
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ConditionalBlock />
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+
+    return (
+      <div>
+        <h4>Listas de objetos</h4>
+        <ul> { cars.map(car => {
+                return (
+                  <li key={car.id}>
+                    <p><strong>Nombre: </strong>{car.name}</p>
+                    <p><strong>Marca: </strong>{car.company}</p>
+                  </li>
+                ) 
+              })
+            }
+        </ul>
+      </div>
+    );
+  }
+} 
 
 export default App;
